8276c4d1c808a0e293437fb042c881055e01f0bd
Brady <bradyrcotton@gmail.com>
2021-06-04 13:55:53 -0500

    created method to retreave the shooter for local storage

--- src/Components/Dope/dope.js
+++ src/Components/Dope/dope.js
@@ -10,9 +10,43 @@ class Dope extends Component {
             scopeAdjustment: 0,
             distance: 0,
             currentZero: 0,
+            shooter: 0,
          }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        
+        let pshooter = localStorage.getItem('shooter');
+        let shooter = parseInt(pshooter)
+        this.setState({shooter: shooter});
+        setTimeout(() => {
+            console.log(
+                'timeout',
+                this.state.shooter
+                );
+            }, 1000);
+            debugger;
+            
+            // console.log(
+            //     'inside callback',
+            //     this.state.shooter
+            //     )
+            // );
+            // console.log(
+            //     'outside callback',
+            //     this.state.shooter
+            //     );
+    };
+    
+    // componentDidUpdate(prevProps, prevState){
+    //     debugger;
+    //     console.log(
+    //         'component did update',
+    //         this.state.shooter
+    //     )
+    
+    // }
+    async createNewDope(dope){
+        await axios.post('http://127.0.0.1:8000/rifle/',dope);
     }
     handleChange(event) {
         this.setState({
@@ -21,18 +55,26 @@ class Dope extends Component {
     }
     handleSubmit(event){
         event.preventDefault();
-        const rifle = {
+        const dope = {
             caliber:this.state.caliber,
             scopeAdjustment:this.state.scopeAdjustment,
             distance: this.state.distance,
             currentZero:this.state.currentZero,
+            shooter: this.state.shooter
+
         }
-        this.createNewRifle(rifle)
+        console.log('dope',dope)
+        this.createNewDope(dope)
         
     }
     
     
     render() { 
+        
+        console.log(
+                'outside callback',
+                this.state.shooter
+                );
         return ( <div>
             <form className='blackbox-form' onSubmit={this.handleSubmit}>
                 <table>
